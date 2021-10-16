a0060319540f58b5a79149acc136a2ca4047f6b0
Chetan <c27801j@gmail.com>
2021-10-16 10:40:23 +0530

    day5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/login_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -20,7 +21,8 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         "/": (context) => LoginPage(),
-        "/login": (context) => HomePage(),
+        MyRoutes.homeRoute: (content) => HomePage(),
+        MyRoutes.loginRoute: (content) => LoginPage(),
       },
     );
   }
